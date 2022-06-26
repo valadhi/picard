653bd304dc03371e4ea01b5eaef9c9998cb54f69
valadhi <vladap3@sgmail.com>
2022-06-26 16:08:10 +0300

    update reset scripts

--- copy_server_files.sh
+++ copy_server_files.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 GIT_HEAD_REF=$(git ls-remote https://github.com/ServiceNow/picard HEAD | awk '{ print $1}')
 CONTAINER_ID=$(docker ps -q --filter ancestor=tscholak/text-to-sql-eval:$GIT_HEAD_REF)
-docker cp ./seq2seq $CONTAINER_ID:/app/seq2seq/
+docker cp ./seq2seq/. $CONTAINER_ID:/app/seq2seq/
+docker exec $CONTAINER_ID /bin/bash -c "pkill -f serve_seq2seq.py"
 docker exec $CONTAINER_ID /bin/bash -c "python seq2seq/serve_seq2seq.py configs/serve.json"
