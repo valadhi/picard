88d084b7a0e88980bc13d414e5e044bc82f47b9f
valadhi <vladap3@sgmail.com>
2022-06-26 15:21:25 +0300

    always retrieve latest hash from main repo

--- copy_server_files.sh
+++ copy_server_files.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-#GIT_HEAD_REF=$(git rev-parse HEAD)
-GIT_HEAD_REF=6a252386bed6d4233f0f13f4562d8ae8608e7445
+GIT_HEAD_REF=$(git ls-remote https://github.com/ServiceNow/picard HEAD | awk '{ print $1}')
 CONTAINER_ID=$(docker ps -q --filter ancestor=tscholak/text-to-sql-eval:$GIT_HEAD_REF)
 docker cp ./seq2seq $CONTAINER_ID:/app/seq2seq/
 docker exec $CONTAINER_ID /bin/bash -c "python seq2seq/serve_seq2seq.py configs/serve.json"
